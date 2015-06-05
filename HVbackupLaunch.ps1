f140578b12d1c2563efac7d791be20274fd83a7a
nelsh <env@virtech.ru>
2015-06-05 15:00:43 +0800

    Add reading from ini-file

---
+++ HVbackupLaunch.ps1
@@ -0,0 +1,13 @@
+﻿# STEP 1. Read parameters from ini-file and set default values
+$inifile = Join-Path $PSScriptRoot ( $MyInvocation.MyCommand.Name.Replace("ps1", "ini") )
+if (!(Test-Path $inifile)) {
+    echo("INI-file not found ({0}).`r`nRTFM`r`n" -f $inifile)
+    exit(0)
+}
+$ini = ConvertFrom-StringData((Get-Content $inifile) -join "`n")
+if (!($ini.ContainsKey("LOGPATH"))) { 
+    $ini.Add("LOGPATH", $PSScriptRoot) # whereis this script
+}
+if (!($ini.ContainsKey("RPTPATH"))) { 
+    $ini.Add("RPTPATH", $PSScriptRoot) # whereis this script
+}
