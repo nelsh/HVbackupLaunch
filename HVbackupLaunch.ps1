3eb04d9395c2aeeb830d4fc1511a47526357f862
nelsh <env@virtech.ru>
2015-06-05 16:00:48 +0800

    Add reading command line parameters

--- HVbackupLaunch.ps1
+++ HVbackupLaunch.ps1
@@ -1,4 +1,17 @@
-﻿# STEP 1. Read parameters from ini-file and set default values
+﻿# STEP 1. Read parameters from command line
+param($p,$m) 
+if (!$p -or !$m) {
+    echo "HVbackup Launcher (http://hypervbackup.codeplex.com/)`n
+`tUsage:`n
+`tHVbackupLaunch -p <path/to/backup/folder> -m <list,of,virtuals,machines>`n`n"
+    exit(0)
+}
+if (!(Test-Path $p)) {
+    echo("Backup folder {0} not exist.`r`n" -f $p)
+    exit(0)
+}
+
+# STEP 2. Read parameters from ini-file and set default values
 $inifile = Join-Path $PSScriptRoot ( $MyInvocation.MyCommand.Name.Replace("ps1", "ini") )
 if (!(Test-Path $inifile)) {
     echo("INI-file not found ({0}).`r`nRTFM`r`n" -f $inifile)
@@ -17,7 +30,7 @@ if (!($ini.ContainsKey("LOGSTORETIME"))) {
     exit(0)
 }
 
-# STEP 2. Remove old logfiles (YYYYMMDD.log)
+# STEP 3. Remove old logfiles (YYYYMMDD.log)
 $logfiles = Get-ChildItem $ini["LOGPATH"] | Where-Object {$_.Name -match "^\d{8}.log$"}
 $logstoredate = (Get-Date).AddMonths(-$ini["LOGSTORETIME"]).ToString('yyyyMMdd')
 foreach ( $l in $logfiles ) {
@@ -26,7 +39,7 @@ foreach ( $l in $logfiles ) {
     }
 }
 
-# STEP 3. Create logfile (
+# STEP 4. Create logfile (
 $log = Join-Path $ini["LOGPATH"] ( (Get-Date).ToString('yyyyMMdd') + ".log" )
 if (!(Test-Path $log)) {
     New-Item -type file $log -force
@@ -41,6 +54,8 @@ $report = Join-Path $ini["RPTPATH"] "report.txt"
 New-Item -type file $report -force
 
 LogWrite("INFO`tStart")
+LogWrite("INFO`tBackup {0} virtual machine(s) ({1}) to {2}"`
+    -f $m.count, ($m -join ', '), $p)
 
 
 
